75327dddf4fc9111df68fc68173d06c2047659c6
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-11-30 19:40:43 +0100

    commit

--- README.md
+++ README.md
@@ -14,11 +14,21 @@ docker exec -it postgres-monolith psql -U admin -d postgres -c "CREATE DATABASE 
 ```
 touch .env .env.test
 
+Example .env file:
+
 CORS_ALLOW_ORIGINS=http://localhost:3000
 OAUTH_CLIENT_ID=client-name
 OAUTH_CLIENT_SECRET=...
 OAUTH_AUTH_URL=https://example.com/auth/realms/client-name/protocol/openid-connect/auth
 OAUTH_TOKEN_URL=https://example.com/auth/realms/client-name/protocol/openid-connect/token
 OAUTH_INTROSPECT_URL=https://example.com/auth/realms/client-name/protocol/openid-connect/token/introspect
+REDIS_PASSWORD=redis
+REDIS_HOST=localhost
+REDIS_PORT=6379
+POSTGRES_HOST=localhost
+POSTGRES_USER=admin
+POSTGRES_PASSWORD=admin
+POSTGRES_DB=chaincue-real-estate-db
+POSTGRES_PORT=5432
 ```
 
--- internal/configs/connect_db.go
+++ internal/configs/connect_db.go
@@ -4,9 +4,12 @@ import (
 	"chaincue-real-estate-go/internal/models"
 	"context"
 	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
+	"os"
 )
 
 var postgresDB *gorm.DB
@@ -21,11 +24,25 @@ func GetRedisClient() *redis.Client {
 	return redisClient
 }
 
-/*TODO env postgres redis*/
 func ConnectPostgres() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("Error loading .env file: ", err)
+	}
+
+	postgresHost := os.Getenv("POSTGRES_HOST")
+	postgresUser := os.Getenv("POSTGRES_USER")
+	postgresPassword := os.Getenv("POSTGRES_PASSWORD")
+	postgresDBName := os.Getenv("POSTGRES_DB")
+	postgresPort := os.Getenv("POSTGRES_PORT")
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		postgresHost, postgresUser, postgresPassword, postgresDBName, postgresPort,
+	)
+
 	var err error
 	postgresDB, err = gorm.Open(postgres.New(postgres.Config{
-		DSN:                  "host=localhost user=admin password=admin dbname=chaincue-real-estate-db port=5432 sslmode=disable",
+		DSN:                  dsn,
 		PreferSimpleProtocol: true,
 	}), &gorm.Config{})
 	if err != nil {
@@ -46,9 +63,18 @@ func ConnectPostgres() {
 }
 
 func ConnectRedis() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("Error loading .env file: ", err)
+	}
+
+	redisPassword := os.Getenv("REDIS_PASSWORD")
+	redisHost := os.Getenv("REDIS_HOST")
+	redisPort := os.Getenv("REDIS_PORT")
+
+	redisAddr := fmt.Sprintf("%s:%s", redisHost, redisPort)
 	redisClient = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
+		Addr:     redisAddr,
+		Password: redisPassword,
 		DB:       0,
 	})
 
